81435ccdf145c72cc710b23a1fe7b02a971c0844
Emre Gümüş <emre.gumus@hepsipay.local>
2017-09-29 14:06:45 +0300

    Hepsipay Java client library added.

---
+++ install_jar_to_local_maven_repo.bat
@@ -0,0 +1,3 @@
+mvn install:install-file -Dfile=hepsipay-java-client-1.1.1.jar -DgroupId=com.hepsipay.client -DartifactId=hepsipay-java-client -Dversion=1.1.1 -Dpackaging=jar
+
+pause
---
+++ install_jar_to_local_maven_repo.sh
@@ -0,0 +1,1 @@
+mvn install:install-file -Dfile=hepsipay-java-client-1.1.1.jar -DgroupId=com.hepsipay.client -DartifactId=hepsipay-java-client -Dversion=1.1.1 -Dpackaging=jar
